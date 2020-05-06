4bb32aa86b917c143e4138f5e237241734ef71f4
khalilsaidane <khalilsaidane96@hotmail.com>
2020-05-06 14:14:50 +0100

    Tweet Form by Hand

---
+++ todo.md
@@ -0,0 +1,22 @@
+1. Users
+    -> Register
+    -> Login
+    -> Profile
+        -> Image (Media storage server)
+        -> Text
+    -> Feed
+        -> User's feed only?
+        -> User + who they follow
+2. Tweets
+    -> Creating
+        -> Text
+        -> Image
+    -> Deleting
+    -> Retweeting
+    -> Linking
+
+
+Long term todos
+    - Notifications
+    - DM
+    - Explore finding hashtags 
---
+++ tweets/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from .models import Tweet
+import random
+from .forms import TweetForm
+
+
+def home_view(request, *args, **kwargs):
+    return render(request, 'pages/home.html', context={}, status=200)
+
+
+def tweet_detail(request, tweet_id, *args, **kwargs):
+    obj = get_object_or_404(Tweet, id=tweet_id)
+    print('-------', obj)
+    data = {
+        'id': obj.id,
+        'content': obj.content,
+    }
+    return JsonResponse(data)
+
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{'id': x.id, 'content': x.content, 'likes': random.randint(0, 10)} for x in qs]
+    data = {
+        'response': tweets_list
+
+    }
+    return JsonResponse(data)
+
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get('next') or None
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        form = TweetForm()
+        if next_url != None:
+            return redirect(next_url)
+    return render(request, 'components/form.html', context={'form': form})
